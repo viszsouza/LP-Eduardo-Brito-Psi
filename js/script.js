232dbc7077691118cffbd79e10699f0272f2db8b
vizssouza <vini.nelso@gmail.com>
2024-07-01 22:40:31 -0300

    Arrumando navlist

--- js/script.js
+++ js/script.js
@@ -21,5 +21,7 @@ function menu(){
 // HAMBURGUER
 const hamburguer = document.querySelector(".hamburguer");
 const nav = document.querySelector(".nav");
+const navlist = document.querySelector(".nav-list")
 
+navlist.addEventListener("click", () => nav.classList.toggle("active"));
 hamburguer.addEventListener("click", () => nav.classList.toggle("active"));
